d7f59d3dd70bf7127c8420012261cb3e3fffef7b
tommy88888 <tommydewa08@gmail.com>
2024-08-04 20:09:06 +0700

    add csrftoken

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,8 +1,11 @@
 import LoginForm from './login-form';
+import { cookies } from 'next/headers';
 
 const LoginPage = () => {
+  const csrfToken = cookies().get('authjs.csrf-token')?.value ?? '';
   return (
     <div className='flex items-center justify-center vertical-center'>
+      <input type='hidden' name='csrfToken' value={csrfToken} />
       <LoginForm />
     </div>
   );
